16effd8af7c12b008f63452bc440bacafce48d25
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-07 11:40:42 -0600

    Test: Add tests for error handling

--- spec/poros/air_quality_spec.rb
+++ spec/poros/air_quality_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe AirQuality, type: :poro do
-  it "exists" do
+  it "can instantiate with given attributes" do
     data = {
       overall_aqi: 20,
       "PM2.5": {
@@ -22,8 +22,34 @@ RSpec.describe AirQuality, type: :poro do
     expect(air_quality).to be_a(AirQuality)
     expect(air_quality.id).to be_nil
     expect(air_quality.city).to eq("Denver")
-    expect(air_quality.aqi).to eq(20)
+    expect(air_quality.aqi).to eq(20.0)
     expect(air_quality.pm25_concentration).to eq(0.5)
     expect(air_quality.co_concentration).to eq(0.1)
+  end
+
+  it "can instantiate with given attributes with nil values" do
+    data = {
+      overall_aqi: nil,
+      "PM2.5": {
+        concentration: nil
+      },
+      CO: {
+        concentration: nil
+      }
+    }
+
+    params = {
+      data: data,
+      city: "Denver"
+    }
+
+    air_quality = AirQuality.new(params)
+
+    expect(air_quality).to be_a(AirQuality)
+    expect(air_quality.id).to be_nil
+    expect(air_quality.city).to eq("Denver")
+    expect(air_quality.aqi).to be_nil
+    expect(air_quality.pm25_concentration).to be_nil
+    expect(air_quality.co_concentration).to be_nil
   end
 end
