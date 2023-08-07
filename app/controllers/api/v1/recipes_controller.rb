728aef6414e1e124f231e120d8d81cb702e94482
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-07 09:06:54 -0600

    Feat: Add RecipesController

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::RecipesController < ApplicationController
+  def index
+    recipe_service = RecipeService.new
+
+    if params[:country]
+      country = params[:country]
+    else
+      country = CountryService.new.random_country
+    end
+
+    recipes = recipe_service.find_recipe_by_country(country)
+
+    recipes = recipes[:hits].map do |recipe|
+      Recipe.new(recipe[:recipe])
+    end
+
+    render json: RecipeSerializer.new(recipes)
+  end
+end
