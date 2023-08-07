4caa4e6b9f03f9d8ec4baa5165299fd1960a5119
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-07 09:07:17 -0600

    Feat: Add RecipeService

---
+++ app/services/recipe_service.rb
@@ -0,0 +1,19 @@
+class RecipeService
+  def find_recipe_by_country(country)
+    get_url("?type=public&q=#{country}")
+  end
+
+  private
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.edamam.com/api/recipes/v2") do |faraday|
+      faraday.params["app_id"] = ENV["EDAMAM_APP_ID"]
+      faraday.params["app_key"] = ENV["EDAMAM_APP_KEY"]
+    end
+  end
+end
