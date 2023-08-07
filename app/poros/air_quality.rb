71af0a9c8675a67a14bcd1e998cf51db1dcb8d1e
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-07 11:40:10 -0600

    Fix: Add error handling for insufficient data.

--- app/poros/air_quality.rb
+++ app/poros/air_quality.rb
@@ -1,15 +1,34 @@
 class AirQuality
   attr_reader :id,
-              :city,
-              :aqi,
-              :pm25_concentration,
-              :co_concentration
+              :city
 
   def initialize(params)
     @id = nil
     @city = params[:city]
-    @aqi = params[:data][:overall_aqi]
-    @pm25_concentration = params[:data][:"PM2.5"][:concentration]
-    @co_concentration = params[:data][:CO][:concentration]
+    @params = params
+  end
+
+  def aqi
+    if @params[:data][:overall_aqi]
+      @params[:data][:overall_aqi].to_f
+    else
+      nil
+    end
+  end
+
+  def pm25_concentration
+    if @params[:data][:"PM2.5"][:concentration]
+      @params[:data][:"PM2.5"][:concentration].to_f
+    else
+      nil
+    end
+  end
+
+  def co_concentration
+    if @params[:data][:CO][:concentration]
+      @params[:data][:CO][:concentration].to_f
+    else
+      nil
+    end
   end
 end
