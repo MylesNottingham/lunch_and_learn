aa34f1f5cc160b31f9ecffb6d3ee6ebcdb0c9207
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-08 13:21:43 -0600

    Feat: Create ImageService

---
+++ app/services/image_service.rb
@@ -0,0 +1,18 @@
+class ImageService
+  def find_images_by_country(country)
+    get_url("search/photos/?query=#{country}&client_id=#{ENV["UNSPLASH_API_KEY"]}")
+  end
+
+  private
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.unsplash.com/") do |faraday|
+      faraday.params["Accept-Version"] = "v1"
+    end
+  end
+end
