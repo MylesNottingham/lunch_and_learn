e363287329a240f9508307d60d80e3abcaaef63f
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-08 17:28:28 -0600

    Feat: Add private method to parse user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,4 +8,10 @@ class Api::V1::UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def params
+    JSON.parse(request.body.read, symbolize_names: true)
+  end
 end
