80bfb62c079c3824a82d24f026ac69ea2e23d723
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-08 22:23:33 -0600

    Test: Add validation of uniqueness scoped to user

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -8,7 +8,31 @@ RSpec.describe Favorite, type: :model do
   describe "validations" do
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:country) }
-    it { should validate_presence_of(:recipe_link) }
     it { should validate_presence_of(:recipe_title) }
+    it { should validate_presence_of(:recipe_link) }
+
+    let(:user) do
+      User.create!(
+        name: "Odell",
+        email: "goodboy@ruffruff.com",
+        password: "treats4lyf",
+        password_confirmation: "treats4lyf"
+      )
+    end
+
+    let(:favorite) do
+      Favorite.create!(
+        user_id: user.id,
+        country: "thailand",
+        recipe_link: "https://www.tastingtable.com/cook/recipes/crab-fried-rice-recipe-thai-food",
+        recipe_title: "Crab Fried Rice (Khaao Pad Bpu)"
+      )
+    end
+
+    it 'validates uniqueness of recipe_link within the scope of user_id' do
+      favorite
+
+      should validate_uniqueness_of(:recipe_link).scoped_to(:user_id)
+    end
   end
 end
