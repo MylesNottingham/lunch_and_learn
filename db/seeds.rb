420e025f13aa21281ca18208b5a3ade0235599c2
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-08 21:18:48 -0600

    Test: Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+User.create!(
+    "name": "Odell",
+    "email": "goodboy@ruffruff.com",
+    "password": "treats4lyf",
+    "password_confirmation": "treats4lyf"
+  )
+  
