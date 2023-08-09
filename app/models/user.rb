12644b14e442d5d52e53141b6fc92b1bb92142bc
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-08 21:24:45 -0600

    Feat: Add favorite relationship to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_token :api_key
   has_secure_password
 
+  has_many :favorites
+
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password_digest, presence: true
