b664e9c24369087144813831eee39f75ae15a454
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-08 23:12:12 -0600

    Test: Add aother User and Favorites to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,46 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 User.destroy_all
+Favorite.destroy_all
 
-User.create!(
+user1 = User.create!(
   "name": "Odell",
   "email": "goodboy@ruffruff.com",
   "password": "treats4lyf",
   "password_confirmation": "treats4lyf"
+)
+
+user2 = User.create!(
+  "name": "Testarossa",
+  "email": "test@testy.com",
+  "password": "test",
+  "password_confirmation": "test"
+)
+
+Favorite.create!(
+  user_id: user1.id,
+  country: "thailand",
+  recipe_link: "https://www.tastingtable.com/cook/recipes/crab-fried-rice-recipe-thai-food",
+  recipe_title: "Crab Fried Rice (Khaao Pad Bpu)"
+)
+
+Favorite.create!(
+  user_id: user1.id,
+  country: "new+zealand",
+  recipe_link: "http://www.marthastewart.com/355892/linguine-new-zealand-cockles",
+  recipe_title: "Linguine with New Zealand Cockles recipes"
+)
+
+Favorite.create!(
+  user_id: user1.id,
+  country: "korea",
+  recipe_link: "https://food52.com/recipes/25423-the-nonsensical-hot-peppercorn-peanut-brittle",
+  recipe_title: "The Nonsensical Hot Peppercorn Peanut Brittle"
+)
+
+Favorite.create!(
+  user_id: user2.id,
+  country: "korea",
+  recipe_link: "https://food52.com/recipes/25423-the-nonsensical-hot-peppercorn-peanut-brittle",
+  recipe_title: "The Nonsensical Hot Peppercorn Peanut Brittle"
 )
